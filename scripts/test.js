fd021044d3da3c3869a61b9e75c4cac31f0f7aa8
Leyla5632 <leyla-5632@mail.ru>
2018-06-16 01:53:30 +0300

    updated test

--- scripts/test.js
+++ scripts/test.js
@@ -29,8 +29,8 @@ $(function () {
 		/* 2 */
 		{
 			name: 'year_birth',
-			title: 'Дата рождения:',
-			placeholder: 'гггг-мм-дд',
+			title: 'Год рождения:',
+			placeholder: 'гггг',
 			type: 'input',
 		},
 		/* 3 */
@@ -144,7 +144,7 @@ $(function () {
 			title: 'Занятия спортом или фитнесом:',
 			type: 'radio',
 			answers: [ 
-				{ text: 'Занимаюсь не менее 1 раза в неделю', count: '2' },
+				{ text: 'Занимаюсь более 1 раза в неделю', count: '2' },
 				{ text: 'Не занимаюсь', count: '-1' },
 			],
 		},
@@ -155,6 +155,7 @@ $(function () {
 			type: 'radio',
 			answers: [ 
 				{ text: 'Здоровое', count: '2' },
+				{ text: 'Обычное', count: '0', warning: 'healthyfood' },
 				{ text: 'Не здоровое', count: '-2', warning: 'healthyfood' },
 			],
 		},
@@ -174,17 +175,17 @@ $(function () {
 			title: 'Как часто Вы болеете простудными заболеваниями?',
 			type: 'radio',
 			answers: [ 
-				{ text: 'Больше 2-х раз в год', count: '-1' },
-				{ text: 'Меньше 2-х раз в год', count: '1' },
+				{ text: 'Больше 4-х раз в год', count: '-1' },
+				{ text: 'Меньше 4-х раз в год', count: '1' },
 			],
 		},
 		/* 15 */
 		{
 			name: 'dead',
-			title: 'Причины смерти кровных родственников (родители, дедушки, бабушки, братья, сестры )',
+			title: 'Чем болели кровные родственники (родители, дедушки, бабушки, братья, сестры )',
 			type: 'checkbox',
 			answers: [ 
-				{ text: 'Все живы или не знаю причин', count: '0' },
+				{ text: 'Ничем или не знаю', count: '0' },
 				{ text: 'Сердце', count: '-2', warning: 'healthyheart' },
 				{ text: 'Инсульт', count: '-2' },
 				{ text: 'Рак', subcheckbox: [
@@ -233,7 +234,9 @@ $(function () {
 	// События
 	function initHandlers () {
 	
-		startTest();
+			
+			startTest();
+		
 		// Клик вне конента модального окна
 		$(modal_test).click(function (e) {
 			closeModalTest();
@@ -295,7 +298,7 @@ $(function () {
 			one_q.placeholder = (one_q.placeholder == undefined) ? (one_q.title +'...') : one_q.placeholder;
 			if(one_q.name == 'year_birth'){
 				$(choice_testing).append('<input type="text" id="year_birth" name="input_test" placeholder="'+ one_q.placeholder +'" />');
-				$('#year_birth').mask("9999-99-99");
+				$('#year_birth').mask("9999");
 			}
 			else{
 				$(choice_testing).append('<input type="text" name="input_test" placeholder="'+ one_q.placeholder +'" />');
@@ -392,15 +395,28 @@ $(function () {
 						}
 					}
 				}
-
 			break;
+
 			case 'input' :
 				val = $('input[name="input_test"]').val();
 				if (val == '') {
 					showErrorTest('Заполните поле');
 					return false;
 				}
+				if (one_q.name == "year_birth" && (val < 1900 || val > 2018)) {
+					showErrorTest('Введено не реальное значение');
+					return false;
+				}
+				if (one_q.name == "weight" && (val < 10 || val > 200)) {
+					showErrorTest('Введено не реальное значение');
+					return false;
+				}
+				if (one_q.name == "height" && (val < 50 || val > 250)) {
+					showErrorTest('Введено не реальное значение');
+					return false;
+				}
 			break;
+			
 			case 'checkbox' :
 				var tmp = [];
 				var elems = $(choice_testing).find('input:checkbox');
