595b13266ec0d8db5e55eb70ac8d1245c1d9a3fb
pauline <pauline-sh-hub@yandex.ru>
2018-06-16 16:00:57 +0300

    added yandex, mail, google metrics

--- test.php
+++ test.php
@@ -142,6 +142,57 @@
 	<link rel="stylesheet" type="text/css" href="css/multiselect.css" />
 	<script src="scripts/multiselect.js"></script>
 	
+	<!-- Yandex.Metrika counter --> 
+	<script type="text/javascript">
+		(function (d, w, c) { 
+			(w[c] = w[c] || []).push(function() { 
+				try { w.yaCounter40933314 = new Ya.Metrika({
+					id:40933314, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true 
+					}); } catch(e) { } 
+		}); 
+		var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { 
+			n.parentNode.insertBefore(s, n); 
+		}; 
+		s.type = "text/javascript"; 
+		s.async = true; 
+		s.src = "https://mc.yandex.ru/metrika/watch.js"; 
+		if (w.opera == "[object Opera]") { 
+			d.addEventListener("DOMContentLoaded", f, false); 
+		} 
+		else { f(); } 
+	})(document, window, "yandex_metrika_callbacks");
+	</script> 
+	<noscript><div><img src="https://mc.yandex.ru/watch/40933314" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
+	<!-- /Yandex.Metrika counter -->   
+
+	<!-- Rating@Mail.ru counter -->
+	<script type="text/javascript">
+	var _tmr = window._tmr || (window._tmr = []);
+	_tmr.push({id: "2885114", type: "pageView", start: (new Date()).getTime()});
+	(function (d, w, id) {
+		if (d.getElementById(id)) return;
+		var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+		ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+		var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+		if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+	})(document, window, "topmailru-code");
+	</script><noscript><div>
+	<img src="//top-fwz1.mail.ru/counter?id=2885114;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+	</div></noscript>
+	<!-- //Rating@Mail.ru counter -->
+	
+	<!-- Google analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-96242372-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- /Google analytics -->
+
 		
 </head>
 <body>
