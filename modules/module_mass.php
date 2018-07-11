3e284dabceee4eccb27591a45d89b66636977024
Leonidas <kirpichik61@gmail.com>
2018-07-11 20:33:50 +0300

    fixed '0 - 0' weight

--- modules/module_mass.php
+++ modules/module_mass.php
@@ -2,7 +2,12 @@
 
 	<div class="icon_mass-index icon_rec"></div>
 
-	<div>Индекс массы вашего тела составляет <?php echo round($index_mass, 0); ?> при норме 20-25, что <br> соответствует <?php echo $txt_index_mass; ?>. Ваш оптимальный вес <?php echo round(pow($result_test['height']/100, 2)*20 , 0, PHP_ROUND_HALF_DOWN); ?>-<?php echo round(pow($result_test['height']/100, 2)*24 , 0, PHP_ROUND_HALF_DOWN); ?> кг</div>
+	<div>Индекс массы вашего тела составляет <?php echo round($index_mass, 0); ?> при норме 20-25, что <br> соответствует <?php echo $txt_index_mass; ?>.
+	 Ваш оптимальный вес <?php if($user->is_logged_in()) {
+		 							echo (round(pow($general_data_row['user_height']/100, 2)*20 , 0, PHP_ROUND_HALF_DOWN) . "-" . round(pow($general_data_row['user_height']/100, 2)*24 , 0, PHP_ROUND_HALF_DOWN));
+	 							} else {
+									echo (round(pow($_SESSION['result_test']['height']/100, 2)*20 , 0, PHP_ROUND_HALF_DOWN) . "-" . round(pow($_SESSION['result_test']['height']/100, 2)*24 , 0, PHP_ROUND_HALF_DOWN));
+								} ?> кг</div>
 	<p>Рекомендуем воспользоваться одной из наших программ по контролю за весом</p>
 	<div class="plus_life help">+ <?php echo -(int)$lifetime_index_mass ?> года жизни</div>
 
