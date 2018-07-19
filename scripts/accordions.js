caac8c33eea90de6ca3dfc8b215d6fa91939e81d
Bryem <pauline-sh-hub@yandex.ru>
2018-07-19 23:13:16 +0300

    accordions open from external link

--- scripts/accordions.js
+++ scripts/accordions.js
@@ -1,23 +1,33 @@
+function open_acc(acc, panel, acc_text){
+    acc.toggleClass("acc-active"); //переворачиваем стрелочку справа
+    acc_text.text(acc_text.text() == "Открыть" ? "Скрыть" : "Открыть");
+
+    if (panel.css("max-height") == "0px") {
+        panel.css("max-height", panel[0].scrollHeight + "px");
+    } else {
+        panel.css("max-height", "0px");
+    }
+}
+
 $(document).ready(function() {
     let accordions = $(".accordion");
 
-    accordions.each(function(index) {
-        var hash = window.location.hash;
-        
+    let hash = window.location.hash;
+    if(hash != ""){
+        let acc = $(hash),
+            panel = acc.next(),
+            acc_text = acc.children().children(".accordions-right");        
 
+        open_acc(acc, panel, acc_text);
+    }
+
+    accordions.each(function(index) {
         let acc = $(this),
             panel = acc.next(),
             acc_text = acc.children().children(".accordions-right");
 
         acc.click(function() {
-            acc.toggleClass("acc-active"); //переворачиваем стрелочку справа
-            acc_text.text(acc_text.text() == "Открыть" ? "Скрыть" : "Открыть");
-
-            if (panel.css("max-height") == "0px") {
-                panel.css("max-height", panel[0].scrollHeight + "px");
-            } else {
-                panel.css("max-height", "0px");
-            }
+            open_acc(acc, panel, acc_text);
         });
     })
 })
